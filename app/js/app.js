a62875f99a5bc90b85403292dab1528c6ce288a3
renanbronchart <renan.bronchart@gmail.com>
2017-02-24 23:20:29 +0100

    [GuestBook:feature] Add sticky in package.json and init carousel

--- app/js/app.js
+++ app/js/app.js
@@ -2,6 +2,7 @@ require('../scss/style.scss');
 
 // require jQuery in global
 global.$ = require('jquery');
+global.slick = require('slick-carousel');
 
 // Call file js
 $(document).ready(function () {
@@ -9,4 +10,21 @@ $(document).ready(function () {
 
 
   require('./rules.js');
+
+   $('.slider-for').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+    fade: true,
+    asNavFor: '.slider-nav'
+  });
+
+  $('.slider-nav').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    asNavFor: '.slider-for',
+    dots: true,
+    centerMode: true,
+    focusOnSelect: true
+  });
 })
