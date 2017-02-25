268aa3c75ba38454a209fccbeaed8fb01e6a0697
renanbronchart <renan.bronchart@gmail.com>
2017-02-25 12:58:30 +0100

    [News:feature] Add section news and add slick carousel on this

--- app/js/slickSlider.js
+++ app/js/slickSlider.js
@@ -4,54 +4,60 @@ var slickSlider = function() {
   };
 
   var initSliderNews = function() {
-    $('.slider__header').slick({
-      slidesToShow: 7,
-      slidesToScroll: 1,
-      arrows: false,
-      centerMode: true,
-      focusOnSelect: false,
-      asNavFor: '.slider__content',
-      swipeToSlide: true,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3
-          }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1
-          }
-        }
-      ]
-    });
+    $('.section__slider').each(function() {
+      var $this = $(this);
+      var $sliderHeader = $this.find('.slider__header');
+      var $sliderContent = $this.find('.slider__content');
 
-    $('.slider__content').slick({
-      slidesToShow: 3,
-      slidesToScroll: 1,
-      asNavFor: '.slider__header',
-      swipeToSlide: true,
-      dots: false,
-      arrows: false,
-      centerMode: true,
-      focusOnSelect: false,
-      responsive: [
-        {
-          breakpoint: 1024,
-          settings: {
-            slidesToShow: 3
+      $sliderHeader.slick({
+        slidesToShow: 7,
+        slidesToScroll: 1,
+        arrows: false,
+        centerMode: true,
+        focusOnSelect: false,
+        asNavFor: $sliderContent,
+        swipeToSlide: true,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1
+            }
           }
-        },
-        {
-          breakpoint: 768,
-          settings: {
-            slidesToShow: 1
+        ]
+      });
+
+      $sliderContent.slick({
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        asNavFor: $sliderHeader,
+        swipeToSlide: true,
+        dots: false,
+        arrows: false,
+        centerMode: true,
+        focusOnSelect: false,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3
+            }
+          },
+          {
+            breakpoint: 768,
+            settings: {
+              slidesToShow: 1
+            }
           }
-        }
-      ]
-    });
+        ]
+      });
+    })
   };
 
   return {init: init};
