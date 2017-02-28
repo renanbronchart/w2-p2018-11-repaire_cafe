11034071dde28f4efa8aa514645759fff02fc191
Leinad93160 <daniel.dupont@hetic.net>
2017-02-28 19:59:36 +0100

    [menu:css] update the menu

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,18 +1,19 @@
 var menu = function () {
-    var init = function () {
-        $('.burger').click(function () {
-            $('.nav').toggleClass('active');
-            $(this).toggleClass('open');
-        });
-    };
-    var toggleNavList = function () {
-        $('.nav__button').click(function (e) {
-            e.preventDefault();
-            $(this).next('.nav__lists').toggle();
-        })
-    };
-    return {init: init, toggleNavList: toggleNavList}
+  var init = function () {
+    $('.burger').click(function () {
+      $('.nav').toggleClass('active');
+      $(this).toggleClass('open');
+    });
+  };
+  var toggleNavList = function () {
+    $('.nav__button').click(function (e) {
+      $(this).next('.nav__lists').toggle();
+      return false;
+    })
+  };
+
+  toggleNavList();
+  return {init: init, toggleNavList: toggleNavList}
 }();
 
 menu.init();
-menu.toggleNavList();
