20caf9e6c5287846a90e1607a121b33596a89cfa
renanbronchart <renan.bronchart@gmail.com>
2017-03-05 23:38:08 +0100

    [NavAside:bug] Close navAside when resize the page and add no scoll on body when is open

--- app/js/menu.js
+++ app/js/menu.js
@@ -1,12 +1,29 @@
 var menu = function () {
+  var idSetTimeout;
+
   var init = function () {
-    $('.header__burger').click(function () {
+    $('.header__burger').on('click', function () {
       $('.lightbox').toggleClass('active');
       $('.navAside').toggleClass('active');
+      $('body').toggleClass('body--noScroll');
       $(this).toggleClass('open');
     });
+
+    $(window).on('resize', function () {
+      clearTimeout(idSetTimeout);
+      idSetTimeout = setTimeout(closeNavAside, 500);
+    });
+
     toggleNavList();
   };
+
+  var closeNavAside = function() {
+    $('.lightbox.active').removeClass('active');
+    $('.navAside.active').removeClass('active');
+    $('.header__burger.open').removeClass('open');
+    $('body').removeClass('body--noScroll');
+  };
+
   var toggleNavList = function () {
     $('.navAside__button').click(function (e) {
       $(this).next('.navAside__lists').toggle();
